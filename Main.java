26bef9ca582d5cde0e5ecc72820bb3b31a655340
MuhammadBurhan123 <burhantheschoolboy@gmail.com>
2024-10-24 04:12:57 +0500

    Merge code

--- Main.java
+++ Main.java
@@ -1,13 +1,46 @@
 class Main {
     public static void main(String[] args) {
-        LinkList list = new LinkList();
+        LinkList firstList = new LinkList();
 
-        list.insert(10);
-        list.insert(20);
-        list.insert(30);
-        list.insert(40);
+        firstList.insert(10);
+        firstList.insert(20);
+        firstList.insert(30);
+        firstList.insert(40);
 
-        list.displayList();
+        firstList.displayList();
+
+        LinkList secondList = new LinkList();
+
+        secondList.insert(50);
+        secondList.insert(60);
+        secondList.insert(70);
+        secondList.insert(80);
+
+        secondList.displayList();
+
+        LinkList mergedList = mergeLinkList(firstList, secondList);
+
+        mergedList.displayList();
+    }
+
+    public static LinkList mergeLinkList(LinkList firstList, LinkList secondList){
+        if(firstList.head == null){
+            return firstList;
+        }
+
+        if(secondList.head == null){
+            return secondList;
+        }
+
+        Node current = firstList.head;
+
+        while(current.next != null){
+            current = current.next;
+        }
+
+        current.next = secondList.head;
+
+        return firstList;
     }
 }
 
